36c0e5aed0711d5412d719ba78e456be9c5705ba
Trixelit <36838998+Trixelit@users.noreply.github.com>
2024-04-24 14:21:20 +1000

    add node library - remove after implementation on server...

--- website/includes/materialConfig.php
+++ website/includes/materialConfig.php
@@ -2,6 +2,7 @@
 
 <head>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
+    <!--
     <script type="importmap">
         {
           "imports": {
@@ -9,7 +10,9 @@
           }
         }
     </script>
+    -->
     <script type="module">
+        require('@material/web');
         import '@material/web/all.js';
         import {styles as typescaleStyles} from '@material/web/typography/md-typescale-styles.js';
         import '@material/web/button/filled-button.js';
